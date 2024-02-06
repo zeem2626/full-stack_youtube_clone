a88e25bc9cf416f5f6f6d84fc1ac059508d2e421
zeem2626 <zeem1920@gmail.com>
2024-02-06 19:14:28 +0530

    frontned url2 added

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ console.log(process.env.FRONTEND_URL);
 app.use(
    cors({
       origin: [
-        "https://zeem-youtube-tawny.vercel.app",
+        "https://full-stack-youtube-clone-sigma.vercel.app",
          process.env.FRONTEND_URL,
          "http://localhost:4173",
          "http://localhost:5173",
