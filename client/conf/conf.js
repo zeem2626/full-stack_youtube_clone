341293167a955df4af571c3020099df3b81def8d
zeem2626 <zeem1920@gmail.com>
2024-02-07 02:15:01 +0530

    removed api in path

--- client/conf/conf.js
+++ client/conf/conf.js
@@ -1,5 +1,5 @@
 const conf = {
-   API: `${import.meta.env.VITE_SERVER_URL}/api`,
+   API: `${import.meta.env.VITE_SERVER_URL}`,
    VITE_FIREBASE_API_KEY: import.meta.env.VITE_FIREBASE_API_KEY,
    VITE_FIREBASE_AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
    VITE_FIREBASE_PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
