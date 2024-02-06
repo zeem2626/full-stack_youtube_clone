812db0679a608d78e505a2180e7b2d94312a609d
zeem2626 <zeem1920@gmail.com>
2024-02-06 18:46:38 +0530

    frontend url added

--- server/app.js
+++ server/app.js
@@ -14,9 +14,10 @@ console.log(process.env.FRONTEND_URL);
 app.use(
    cors({
       origin: [
+        "https://zeem-youtube-tawny.vercel.app/",
          process.env.FRONTEND_URL,
          "http://localhost:4173",
-        //  "http://localhost:5173",
+         "http://localhost:5173",
       ],
       credentials: true,
    })
